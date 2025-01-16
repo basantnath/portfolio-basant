fbfc465cfa4ea4957600030d324c5bcc0efa722a
I'm Basant <basantnath00@gmail.com>
2025-01-16 22:33:18 +0545

    Update name-animation.js

--- public/js/name-animation.js
+++ public/js/name-animation.js
@@ -1,6 +1,6 @@
 new TypeIt("#typed-text", {
-  strings: ["Web Designer", "Programmer", "Web Developer"],
-  speed: 100,
+  strings: ["I'm a Web Designer", "Programmer", "Web Developer"],
+  speed: 70,
   breakLines: false,
   nextStringDelay: 1000,
   loop: true,
